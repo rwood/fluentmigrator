b5fe18fffb0b11412f4e4c0199375383a9edcb8d
Tony OHagan <tony@ohagan.name>
2014-02-22 00:51:55 +1000

    MigrationVersionAttribute now provides additional helper methods.

--- src/SchemaGenTemplate/FM_Extensions/MigrationVersionAttribute.cs
+++ src/SchemaGenTemplate/FM_Extensions/MigrationVersionAttribute.cs
@@ -14,14 +14,47 @@ namespace Migrations.FM_Extensions
         {
         }
 
+        public const int MaxMajor = 100;
+        public const int MaxMinor = 100;
+        public const int MaxPatch = 10000;
+        public const int MaxStep  = 1000;
+
         private static long CalculateValue(int major, int minor, int patch, int step)
         {
-            Debug.Assert(major < 100);
-            Debug.Assert(minor < 100);
-            Debug.Assert(patch < 100);
-            Debug.Assert(step < 1000);
+            Debug.Assert(major < MaxMajor);
+            Debug.Assert(minor < MaxMinor);
+            Debug.Assert(patch < MaxPatch);    // Can be app build number
+            Debug.Assert(step < MaxStep);
 
-            return ((((major * 100L) + minor) * 100L) + patch) * 1000L + step;
+            return ((((major * MaxMinor) + minor) * MaxPatch) + patch) * MaxStep + step;
+        }
+
+        public void GetVersion(out int major, out int minor, out int patch, out int step)
+        {
+            long version = Version;
+
+            step = (int) (version % MaxStep);
+            version = version / MaxStep;
+
+            patch = (int) (version % MaxPatch);
+            version = version / MaxPatch;
+
+            minor = (int) (version % MaxMinor);
+            major = (int) (version / MaxMinor);
+        }
+
+        public override string ToString()
+        {
+            int major, minor, patch, step;
+            GetVersion(out major, out minor, out patch, out step);
+            return string.Format("{0}-{1}-{2}-{3}", major, minor, patch, step);
+        }
+
+        public string ToStringZeroFilled()
+        {
+            int major, minor, patch, step;
+            GetVersion(out major, out minor, out patch, out step);
+            return string.Format("{0:D2}-{1:D2}-{2:D4}-{3:D3}", major, minor, patch, step);
         }
     }
 }
