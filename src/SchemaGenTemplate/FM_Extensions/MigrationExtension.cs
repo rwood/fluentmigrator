98d875d05efeec51c525df12b0bc6cf22273227c
Tony OHagan <tony@ohagan.name>
2014-02-23 13:49:51 +1000

    Updated SchemaGenTemplate/FM_Extensions/*.cs classes to use additions to FM API. Replaced Delete*() methods with new code in FM API so no longer needing most of the old MigrationExt.cs . Replaced with MigrationExtension.cs

---
+++ src/SchemaGenTemplate/FM_Extensions/MigrationExtension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FluentMigrator;
+
+namespace Migrations.FM_Extensions
+{
+    public static class MigrationExtension
+    {
+        /// <summary>
+        /// A mapping from database type or instance to a SQL script file tag.
+        /// <remarks>
+        /// WARNING: Used by classes generated from FluentMigrator.SchemaGen.SchemaWriters.ExecuteSqlDirectory as well as <see cref="SqlScriptMigration"/>.
+        /// </remarks>
+        /// </summary>
+        public static string GetCurrentDatabaseTag(this Migration migration)
+        {
+            string cs = migration.ConnectionString.ToUpper();
+
+            if (cs.Contains("MICROSOFT.JET.OLEDB") || cs.Contains("MICROSOFT.ACE.OLEDB"))
+            {
+                if (cs.Contains(".MDB"))
+                {
+                    return "AC1";   // Tag for 1st Access Database
+                }
+                else if (cs.Contains(".DAT"))
+                {
+                    return "AC2";   // Tag for 2nd Access Database
+                }
+            }
+            else if (cs.Contains("SERVER="))
+            {
+                return "SS";        // Tag for SQL Server database
+            }
+
+            throw new Exception("Connection string not recognised");
+        }
+
+    }
+}
--- src/SchemaGenTemplate/FM_Extensions/SqlScriptMigration.cs
+++ src/SchemaGenTemplate/FM_Extensions/SqlScriptMigration.cs
@@ -2,29 +2,34 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using FluentMigrator;
 
 namespace Migrations.FM_Extensions
 {
-    public abstract class SqlScriptMigration : MigrationExt
+    public abstract class SqlScriptMigration : Migration
     {
         private readonly string sqlFolder;
         private readonly SearchOption searchOption;
-        private readonly string[] tagsList;
+        private readonly string[] tags;
 
-        internal SqlScriptMigration(string sqlFolder, SearchOption searchOption = SearchOption.AllDirectories, IEnumerable<string> tags = null)
+        internal SqlScriptMigration(string sqlFolder, string tag, SearchOption searchOption = SearchOption.AllDirectories)
+            : this(sqlFolder, new [] { tag }, searchOption)
+        {
+        }
+
+        internal SqlScriptMigration(string sqlFolder, string[] tags = null, SearchOption searchOption = SearchOption.AllDirectories)
         {
             if (sqlFolder == null) throw new ArgumentNullException("sqlFolder");
             this.sqlFolder = sqlFolder;
             this.searchOption = searchOption;
 
-            // Filter SQL files by current database type or instance.
-            tags = tags ?? new string[] { };
-            tagsList = tags.Concat(new string[] { CurrentDatabaseTag }).ToArray();
+            this.tags = tags ?? new string[] { };
         }
 
         public override void Up()
         {
-            Execute.ScriptDirectory(sqlFolder, searchOption, tagsList);
+            // Filter SQL files by current database type or instance.
+            Execute.ScriptsInDirectory(sqlFolder, searchOption).WithTags(tags).WithTag(this.GetCurrentDatabaseTag());
         }
 
         public override void Down()
