132a41222f48bd1b5f3e5fd1fbd24495fed8ddc0
Tony OHagan <tony@ohagan.name>
2014-02-25 23:36:47 +1000

    Moved SQL folder in FluentMigrator.SchemaGen.Test.csproj to SchemaGenTest-SQL to avoid conflict in build/ folder when building whole solution.

--- src/FluentMigrator.SchemaGen/FluentMigrator.SchemaGen.Test/SchemaWriters/SqlFileWriterTests.cs
+++ src/FluentMigrator.SchemaGen/FluentMigrator.SchemaGen.Test/SchemaWriters/SqlFileWriterTests.cs
@@ -18,6 +18,7 @@ namespace FluentMigrator.SchemaGen.Test.SchemaWriters
     [Category("Integration")]
     class SqlFileWriterTests
     {
+        private const string SQL_DIR = "SchemaGenTest-SQL"; // must not be "SQL" to avoid build conflicts.
         private ISqlFileWriter writer;
         private SchemaGenOptions options;
         private IAnnouncer announcer;
@@ -25,7 +26,12 @@ namespace FluentMigrator.SchemaGen.Test.SchemaWriters
         [SetUp]
         public void SetUp()
         {
-            SchemaGenOptions.Instance = options = new SchemaGenOptions { SqlDir = "SQL" };
+            SchemaGenOptions.Instance = options = new SchemaGenOptions
+                {
+                    SqlBaseDir = SQL_DIR,
+                    SqlDir = SQL_DIR
+                };
+
             announcer = new ConsoleAnnouncer();
             writer = new SqlFileWriter(options, announcer);
             options.EmbedSql.ShouldBe(false);   // OFF by default
@@ -49,7 +55,7 @@ namespace FluentMigrator.SchemaGen.Test.SchemaWriters
         [Test]
         public void CanExecuteSqlFile()
         {
-            var lines = writer.ExecuteSqlFile(new FileInfo(@"SQL\sample0.sql")).ToArray();
+            var lines = writer.ExecuteSqlFile(new FileInfo(SQL_DIR + "\\sample0.sql")).ToArray();
             lines.Length.ShouldBe(2);
             lines[0].ShouldBe("");
             lines[1].ShouldBe(@"Execute.Script(""sample0.sql"");");
@@ -59,33 +65,33 @@ namespace FluentMigrator.SchemaGen.Test.SchemaWriters
         public void CanEmbedSqlFile()
         {
             options.EmbedSql = true;            // Turn ON
-            var lines = writer.ExecuteSqlFile(new FileInfo(@"SQL\sample1.sql")).ToArray();
+            var lines = writer.ExecuteSqlFile(new FileInfo(SQL_DIR + "\\sample1.sql")).ToArray();
             lines.Length.ShouldBe(2);
-            lines[0].ShouldBe("// sample1.sql");
-            lines[1].ShouldBe(@"Execute.Sql(@""DROP TABLE [Users]"");");
+            lines[0].ShouldBe(@"Execute.Sql(@""/* sample1.sql */");
+            lines[1].ShouldBe("\tDROP TABLE [Users]\");");
         }
 
         [Test]
         public void WontEmbedSqlFileWhenEmptyStatements()
         {
-            CodeLines lines = writer.EmbedSqlFile(new FileInfo(@"SQL\empty.sql"));
+            CodeLines lines = writer.EmbedSqlFile(new FileInfo(SQL_DIR + "\\empty.sql"));
             lines.Count().ShouldBe(0);
         }
 
         [Test]
         public void CanEmbedTaggedSqlFile()
         {
-            CodeLines lines = writer.EmbedSqlFile(new FileInfo(@"SQL\sample2.sql"));
-            //Assert.Fail(lines.ToString());
+            string[] actual = writer.EmbedSqlFile(new FileInfo(SQL_DIR + "\\sample2.sql")).ToArray();
+            //File.WriteAllLines(@"sample2.txt", actual); // Capture regression to Debug folder
 
             string[] expected = File.ReadAllLines(@"Expected\sample2.txt");
-            lines.Cast<string>().ShouldBe(expected);
+            actual.ShouldBe(expected);
         }
 
         [Test]
         public void CanLinkSqlDirectoryWithTaggedFiles()
         {
-            CodeLines lines = writer.ExecuteSqlDirectory(new DirectoryInfo(@"SQL"));
+            CodeLines lines = writer.ExecuteSqlDirectory(new DirectoryInfo(SQL_DIR));
             lines.Count().ShouldBe(2);
             lines.ToArray()[1].ShouldBe(@"Execute.ScriptsInNestedDirectories(""."").WithTag(this.GetDbTag()).WithGos();");
         }
@@ -94,11 +100,10 @@ namespace FluentMigrator.SchemaGen.Test.SchemaWriters
         public void CanEmbedSqlDirectoryWithTaggedFiles()
         {
             options.EmbedSql = true;            // Turn ON
-            var actual = writer.ExecuteSqlDirectory(new DirectoryInfo(@"SQL\3_Post")).ToArray();
+            var actual = writer.ExecuteSqlDirectory(new DirectoryInfo(SQL_DIR + "\\3_Post")).ToArray();
             //File.WriteAllLines(@"embed-all.txt", actual); // Capture regression to Debug folder
 
             string[] expected= File.ReadAllLines(@"Expected\embed-all.txt");
-
             actual.ShouldBe(expected);
         }
 
