1894af21daa980481e43d24a27dce198859d6fa8
Tony OHagan <tony@ohagan.name>
2014-02-27 14:08:48 +1000

    minor comment change to SqlServer/SqlServer2005TypeMap.cs

--- src/FluentMigrator.Runner/Generators/SqlServer/SqlServer2005TypeMap.cs
+++ src/FluentMigrator.Runner/Generators/SqlServer/SqlServer2005TypeMap.cs
@@ -8,7 +8,7 @@ namespace FluentMigrator.Runner.Generators.SqlServer
         {
             base.SetupTypeMaps();
 
-            // SQL Server 2005+ should use VARCHAR(MAX), NVARCHAR(MAX) and VARBINARY(MAX) which replace TEXT and NTEXT 
+            // SQL Server 2005+ should use VARCHAR(MAX), NVARCHAR(MAX) and VARBINARY(MAX) which replace TEXT and NTEXT and IMAGE 
             SetTypeMap(DbType.String, "NVARCHAR(MAX)", UnicodeTextCapacity);
             SetTypeMap(DbType.AnsiString, "VARCHAR(MAX)", AnsiTextCapacity);
             SetTypeMap(DbType.Binary, "VARBINARY(MAX)", ImageCapacity);
